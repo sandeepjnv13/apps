649f68facade4b3fee6c407d0a7e36be9c0ce5bb
Sandeep <sandeepjnv13@gmail.com>
2025-06-19 23:01:06 +0530

    added first app

---
+++ test/src/main/java/testapp/TestMain.java
@@ -0,0 +1,11 @@
+package testapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMain {
+    public static void main(String[] args) {
+        SpringApplication.run(TestMain.class, args);
+    }
+}
